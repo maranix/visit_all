9870e0b0470a1684c7be58429e4e4ca568c5be8b
ramanverma2k <ramanverma4183@gmail.com>
2021-04-07 23:46:47 +0530

    fetch links from 2nd source

--- source/investing.go
+++ source/investing.go
@@ -1,1 +1,34 @@
 package source
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/ramanverma2k/visit_all/process"
+)
+
+// (.*)
+
+func Investing(url string, keyword string) {
+	homepage := process.PageRequest(&url)
+	re := regexp.MustCompile(`href="/news.*?">`)
+	links_to_visit := re.FindAllString(homepage, -1)
+	filtered_links := process.RemoveDuplicatesUnordered(links_to_visit)
+	for _, v := range filtered_links {
+		fmt.Println(v)
+	}
+	// file, err := os.OpenFile("links.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// writer := bufio.NewWriter(file)
+
+	// for _, data := range filtered_links {
+	// 	_, _ = writer.WriteString(data + "\n")
+	// }
+
+	// writer.Flush()
+	// file.Close()
+	// fmt.Println(process.VisitLinks(&keyword, filtered_links))
+}
