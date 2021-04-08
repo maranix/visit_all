9ed3cede10514d42f6c3b2f20f357becaf2649d2
ramanverma2k <ramanverma4183@gmail.com>
2021-04-08 22:28:31 +0530

    update moneycontrol source function

--- source/moneycontrol.go
+++ source/moneycontrol.go
@@ -2,15 +2,11 @@ package source
 
 import (
 	"fmt"
-	"regexp"
 
 	"github.com/ramanverma2k/visit_all/process"
 )
 
 func MoneyControl(url string, keyword string) {
-	homepage := process.PageRequest(&url)
-	re := regexp.MustCompile(`\s*(?i)https://www[.]moneycontrol[.]com(\"([^"]*\")|'[^']*'|([^'">\s]+))`)
-	links_to_visit := re.FindAllString(homepage, -1)
-	filtered_links := process.RemoveDuplicatesUnordered(links_to_visit)
-	fmt.Println(process.VisitLinks(&keyword, filtered_links))
+	links := process.RegEx(url)
+	fmt.Println(process.VisitLinks(&keyword, links))
 }
