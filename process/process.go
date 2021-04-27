def70d4a4f485de0b2320cc6f08eca26dfbc1364
ramanverma2k <ramanverma4183@gmail.com>
2021-04-27 17:27:12 +0530

    introduce channel and routine to make it go faaaast (WIP)

--- process/process.go
+++ process/process.go
@@ -3,13 +3,12 @@ package process
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"regexp"
 	"strings"
 )
 
-func RemoveDuplicatesUnordered(elements []string, url ...string) []string {
+func RemoveDuplicatesUnordered(elements []string, url string) []string {
     encountered := map[string]bool{}
 
     // Create a map of all unique elements.
@@ -19,11 +18,9 @@ func RemoveDuplicatesUnordered(elements []string, url ...string) []string {
 
     // Place all keys from the map into a slice.
     result := []string{}
-    for key, _ := range encountered {
-        if (url != nil) {
-			result = append(result, url[0]+strings.Trim(key, "=\""))
-		} else {
-			result = append(result, strings.Trim(key, "=\""))
+    for key := range encountered {
+        if (url != "") {
+			result = append(result, url+strings.Trim(key, "=\""))
 		}
     }
     return result
@@ -32,7 +29,7 @@ func RemoveDuplicatesUnordered(elements []string, url ...string) []string {
 func PageRequest(url *string) string {
 	resp, err := http.Get(*url)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
 
 	defer resp.Body.Close()
@@ -44,48 +41,57 @@ func PageRequest(url *string) string {
 
 func RegEx(url string, expression ...string) []string {
 	var re *regexp.Regexp
-	var filtered_links []string
+	var links_to_visit []string
 	homepage := PageRequest(&url)
 
-	if (expression != nil) {
-		re = regexp.MustCompile(expression[0])
-		links_to_visit := re.FindAllString(homepage, -1)
-		filtered_links = RemoveDuplicatesUnordered(links_to_visit)
-	} else {
+	if (expression == nil) {
 		re = regexp.MustCompile(`[^a-z]"/[[:alpha:]].*?"`)
-		links_to_visit := re.FindAllString(homepage, -1)
-		filtered_links = RemoveDuplicatesUnordered(links_to_visit, url)
+		links_to_visit = re.FindAllString(homepage, -1)
+	} else {
+		re = regexp.MustCompile(expression[0])
+		links_to_visit = re.FindAllString(homepage, -1)
 	}
-	
-	
+	filtered_links := RemoveDuplicatesUnordered(links_to_visit, url)
 	return filtered_links
 }
 
-func SubLinks(s []string) []string {
+func SubLinks(s []string, url string) []string {
+	var filtered_links []string
 	s_tmp := s
+	fmt.Println("Before: ", len(s))
 	for _, l := range s_tmp {
-		sublinks := RegEx(l)
-		s = append(s, sublinks...)
+		temp := RegEx(l)
+		s = append(s, temp...)
 	}
-
-	return s
+	
+	s_tmp = s
+	for _, l := range s_tmp {
+		temp := RegEx(l)
+		s = append(s, temp...)
+	}
+	fmt.Println("s_tmp: ",len(s_tmp))
+	fmt.Println("s: ",len(s))
+	filtered_links = RemoveDuplicatesUnordered(s, url)
+	fmt.Println("After: ",len(filtered_links))
+	return filtered_links
 }
 
 func VisitLinks(keyword *string,links []string) int {
+	channel := make(chan int)
 	var sum int
 	for _, l := range links {
-		count := FindKeyword(keyword, PageRequest(&l), l)
-		sum = sum + count
+		go FindKeyword(keyword, PageRequest(&l), l, channel)
+		sum = sum + <-channel
 	}
 	return sum
 }
 
-func FindKeyword(keyword *string, source string, link string) int {
+func FindKeyword(keyword *string, source string, link string, ch chan<-int) {
 	// <(?:h1|h2|h3|p).*?>.*(Plastiblends).*</(?:h1|h2|h3|p)>
 	re := regexp.MustCompile(`<(?:h1|h2|h3|p).*?>.*(`+*keyword+`).*</(?:h1|h2|h3|p)>`)
 	match := re.FindAllString(source, -1)
 	if match != nil {
 		fmt.Println(link, len(match))
 	}
-	return len(match)
+	ch <- len(match)
 }
